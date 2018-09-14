f82b6a1151d2e83f1d88d99826f86ecca32a83cc
Robbie <txt3rob@gmail.com>
2018-09-14 14:25:51 +0100

    Update downloadall.py

--- completed/downloadall.py
+++ completed/downloadall.py
@@ -3,9 +3,9 @@ import requests
 import os
 
 session = requests.Session()
+YOURTOKEN = "YOURAPITOKEN"
 
-
-headers = {"Accept":"*/*","X-Token","YOURTOKEN","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate","Content-Type":"application/x-www-form-urlencoded"}
+headers = {"Accept":"*/*","X-Token":YOURTOKEN,"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate","Content-Type":"application/x-www-form-urlencoded"}
 response = session.get("https://api.binaryedge.io/v1/tasks", headers=headers)
 
 
@@ -17,5 +17,5 @@ for x in y:
   		os.makedirs(x["job_id"])
   		job_id = x["job_id"]
   		print ("[*] Downloading Job ID: "+job_id+" [*]")
-  		os.system('curl -l https://stream.api.binaryedge.io/v1/replay/'+job_id+' -H "X-Token:YOURTOKEN" '+job_id+'/'+job_id+'.json')
+  		os.system('curl -l https://stream.api.binaryedge.io/v1/replay/'+job_id+' -H "X-Token:"'+YOURTOKEN+'" '+job_id+'/'+job_id+'.json')
   		print ("[*] Downloaded Job ID: "+job_id+" [*]")
